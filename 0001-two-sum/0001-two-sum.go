26f1f04a6c2557dc2117129da30bcea7307ab82d
RamiroCruz <27798751+ramirocruz@users.noreply.github.com>
2023-03-21 13:22:36 +0530

    Time: 4 ms (91.24%), Space: 4.4 MB (12.68%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.go
@@ -0,0 +1,12 @@
+func twoSum(nums []int, target int) []int {
+    mp := map[int]int {}
+    for index, value := range nums{
+        rem_value := target - value
+        _, isPresent := mp[rem_value]
+        if isPresent{
+            return []int {index,mp[rem_value]}
+        }
+        mp[value] = index
+    }
+    return []int {}
+}
