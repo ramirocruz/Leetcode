37b436a3899afd1f78ce1d6e6b11b8c603c62d19
RamiroCruz <27798751+ramirocruz@users.noreply.github.com>
2023-04-28 18:27:49 +0530

    Time: 6 ms (69.62%), Space: 10.6 MB (92.34%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-    void helper(vector<int>&candidates, int target, vector<int> &tempdata, int tempsum, int pos,set<vector<int>>&st,vector<vector<int>>&res)
+    void helper(vector<int>&candidates, int target, vector<int> &tempdata, int tempsum, int pos,vector<vector<int>>&res)
     {
         if(tempsum > target)
             return;
         
         if(pos >= candidates.size())
         {
-            // if(st.count(tempdata) == 0)
-            // {
-            //     st.insert(tempdata);
-                if(tempsum == target)
-                    res.push_back(tempdata);
-                
-            // }
+            if(tempsum == target)
+                res.push_back(tempdata);
             return;
         }
         
         tempdata.push_back(candidates[pos]);
-        helper(candidates,target,tempdata,tempsum + candidates[pos],pos,st,res);
+        helper(candidates,target,tempdata,tempsum + candidates[pos],pos,res);
         
         tempdata.pop_back();
-        helper(candidates,target,tempdata,tempsum,pos + 1,st,res);
+        helper(candidates,target,tempdata,tempsum,pos + 1,res);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         
         vector<int>tempdata;
-        set<vector<int>>st;
         vector<vector<int>>res;
-        // sort(candidates.begin(),candidates.end());
-        helper(candidates,target,tempdata,0,0,st,res);
+        helper(candidates,target,tempdata,0,0,res);
         return res;
     }
 };
