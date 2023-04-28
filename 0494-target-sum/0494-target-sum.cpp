94916bcf2a5f414f6fe17f7702b1fb555ce5f1a8
RamiroCruz <27798751+ramirocruz@users.noreply.github.com>
2023-04-28 17:08:26 +0530

    Time: 1501 ms (14.23%), Space: 9 MB (84.53%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int helper(vector<int>&nums,int target,int pos, vector<int>&tempdata,int tempsum)
+    int helper(vector<int>&nums,int target,int pos,int tempsum)
     {
         if(pos == nums.size())
         {
@@ -14,18 +14,14 @@ public:
         
         int count = 0;
         // nums[pos] is added to the sum
-        tempdata.push_back(nums[pos]);
-        count += helper(nums,target,pos+1,tempdata,tempsum + nums[pos]);
+        count += helper(nums,target,pos+1,tempsum + nums[pos]);
         
         // nums[pos] is substracted from the sum
-        tempdata.pop_back();
-        count += helper(nums,target,pos+1,tempdata,tempsum - nums[pos]);
+        count += helper(nums,target,pos+1,tempsum - nums[pos]);
         
         return count;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        
-        vector<int>tempdata;
-        return helper(nums,target,0,tempdata,0);
+        return helper(nums,target,0,0);
     }
 };
