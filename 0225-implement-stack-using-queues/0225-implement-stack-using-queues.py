e0234f900a99cddbce609744a1f3d476c67f16b2
RamiroCruz <27798751+ramirocruz@users.noreply.github.com>
2023-08-28 16:33:06 +0530

    Time: 39 ms (67.59%), Space: 16.4 MB (67.63%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -5,47 +5,29 @@ class MyStack:
     def __init__(self):
         self.q1 = deque()
         self.q2 = deque()
-        self.turn = 0
         
         
 
     def push(self, x: int) -> None:
-        if(self.turn == 0):
-            self.q1.append(x)
-        else:
-            self.q2.append(x)
+        self.q1.append(x)
         
 
     def pop(self) -> int:
+        while(len(self.q1) > 1):
+            tempvar = self.q1.popleft()
+            self.q2.append(tempvar)
+        res = self.q1.popleft()
         print(self.q1,self.q2)
-        if(self.turn == 0):
-            while(len(self.q1) > 1):
-                tempvar = self.q1.popleft()
-                self.q2.append(tempvar)
-            res = self.q1.popleft()
-            self.turn = 1
-            print(self.q1,self.q2)
-            return res
-        else:
-            while(len(self.q2) > 1):
-                tempvar = self.q2.popleft()
-                self.q1.append(tempvar)
-            res = self.q2.popleft()
-            self.turn = 0
-            return res           
+        self.q1,self.q2 = self.q2,self.q1
+        print(self.q1,self.q2)
+        return res           
 
     def top(self) -> int:
-        if(self.turn == 0):
-            return self.q1[-1]
-        else:
-            return self.q2[-1]
+        return self.q1[-1]
         
 
     def empty(self) -> bool:
-        if(self.turn == 0):
-            return (len(self.q1) == 0)
-        else:
-            return (len(self.q2) == 0)
+        return (len(self.q1) == 0)
         
 
 
