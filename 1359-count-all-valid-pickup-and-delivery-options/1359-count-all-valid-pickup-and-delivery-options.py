f85c2533ec643f56c105a262256161efb9f7ca1d
RamiroCruz <27798751+ramirocruz@users.noreply.github.com>
2023-09-11 00:16:01 +0530

    Time: 37 ms (79.15%), Space: 16.2 MB (69.79%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countOrders(self, n: int) -> int:
+        """
+        We can assume, a box of size 2n where at one step we have to fill P1 and D1 at any place.
+        We have thus 2nC2 way to place the first P-D pair. For second pair P2-D2, (2n - 2) vacant 
+        spaces are left. We can thus place the second pair in (2n - 2)C2 ways. Thus so on we can
+        multiply all the ways together to get the total ways.
+        
+        2nC2 x (2n - 2)C2 x (2n - 4)C2 x....x 4C2 x 2C2
+        
+        """
+        M = int(1e9 + 7)
+        ans = 1
+        
+        for i in range(1,n+1):
+            ans = (ans * ((2*i)*(2*i - 1))//2)%M
+        
+        return ans
+        
