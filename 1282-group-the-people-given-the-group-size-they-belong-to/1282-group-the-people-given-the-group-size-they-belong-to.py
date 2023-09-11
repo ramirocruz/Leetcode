622662ec1e11fb9b9d80b39fbdad1f369e27e690
RamiroCruz <27798751+ramirocruz@users.noreply.github.com>
2023-09-11 21:58:29 +0530

    Time: 70 ms (82.92%), Space: 16.3 MB (91.52%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,26 @@
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        freq_dict = {}
+        for idx,el in enumerate(groupSizes):
+            if el in freq_dict:
+                freq_dict[el].append(idx)
+            else:
+                freq_dict[el] = [idx]
+        
+        # print(freq_dict)
+        ans = []
+        for key,val in freq_dict.items():
+            temp = []
+            for elem in val:
+                temp.append(elem)
+                if len(temp) == key:
+                    ans.append(temp)
+                    temp = []
+        
+        
+        return ans
+        
+            
+        
+        
+        
