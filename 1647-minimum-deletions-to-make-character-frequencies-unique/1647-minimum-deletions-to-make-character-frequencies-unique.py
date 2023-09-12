876c470d2c4c0730227c960be905237cb2313055
RamiroCruz <27798751+ramirocruz@users.noreply.github.com>
2023-09-12 21:48:03 +0530

    Time: 243 ms (13.15%), Space: 17.3 MB (23.36%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -5,35 +5,14 @@ class Solution:
         for c in s:
             fcount[ord(c) - ord('a')] += 1
         
-        fcount.sort()
-        used_idx = set({})
-        visited = set({})
-        
+        used = set()
         ans = 0
-        # print(fcount)
+        
         for el in fcount:
-            if(el == 0):
-                continue
-            
-            used_idx.add(el)
-            
-            if el in visited:
-                n = el - 1
-                while n:
-                    if n not in used_idx:
-                        used_idx.add(n)
-                        ans += el - n
-                        break
-                    n -= 1
-                
-                if(n == 0):
-                    ans += el
- 
-            else:
-                visited.add(el)
-                
-        # print(visited)
-        # print(used_idx)
+            while el > 0 and el in used:
+                el -= 1
+                ans += 1
+            used.add(el)
         
         return ans
         
