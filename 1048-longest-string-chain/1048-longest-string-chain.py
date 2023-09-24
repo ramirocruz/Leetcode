9bd9c5138c8a9a0cfd467315c35d3e7cef5632e0
RamiroCruz <27798751+ramirocruz@users.noreply.github.com>
2023-09-24 17:33:13 +0530

    Time: 519 ms (36.65%), Space: 19 MB (15.67%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,6 +1,62 @@
 class Solution:
-       def longestStrChain(self, words):
+    def longestStrChain(self, words: List[str]) -> int:
+        wordlist = {}
+        
+        for word in words:
+            if len(word) not in wordlist:
+                wordlist[len(word)]= [word]
+            else:
+                wordlist[len(word)].append(word)
+        
+        # print(wordlist)
+        
+        lenkeys = list(wordlist.keys())
+        lenkeys.sort()
+        graph = {}
+        
+        visited = {}
+        
+        def appendGraph(keylen):
+            for word in wordlist[keylen]:
+                graph[word] = []
+                for suc_word in wordlist[keylen+1]:
+                    graph[word].append(suc_word)
+ 
+        for i in range(len(lenkeys) - 1):
+            if(lenkeys[i] + 1 == lenkeys[i+1]):
+                appendGraph(lenkeys[i])
+        
+        def isSubsequence(word1,word2):
+            i,j = 0,0
+            while i < len(word1) and j < len(word2):
+                if(word1[i] == word2[j]):
+                    i += 1
+                j += 1
+            
+            if(i == len(word1)):
+                return True
+            return False
         dp = {}
-        for w in sorted(words, key=len):
-            dp[w] = max(dp.get(w[:i] + w[i + 1:], 0) + 1 for i in range(len(w)))
-        return max(dp.values())
+        def dfs(word):
+            if word in dp:
+                return dp[word]
+            if word not in graph:
+                return 1
+            maxlen = 0
+            for w in graph[word]:
+                if(isSubsequence(word,w)):
+                    maxlen = max(maxlen,dfs(w))
+            
+            dp[word] = maxlen + 1
+            return dp[word]
+        
+        ans = 0
+
+        for key in lenkeys[::-1]:
+            for word in wordlist[key]:                
+                ans = max(ans,dfs(word))
+                
+        
+        # print(graph)
+        # print(lenkeys)
+        return ans
