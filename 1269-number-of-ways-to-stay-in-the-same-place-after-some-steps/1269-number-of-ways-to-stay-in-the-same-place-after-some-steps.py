aef4d4a00fa9f86347ebb2bf5b6879a6a92bb550
RamiroCruz <27798751+ramirocruz@users.noreply.github.com>
2023-10-15 11:14:39 +0530

    Time: 404 ms (44.44%), Space: 24.7 MB (61.67%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -1,21 +1,28 @@
 class Solution:
     def numWays(self, steps: int, arrLen: int) -> int:
         
-        # dp = [[-1]*501 for _ in range(arrLen + 1) ]
         M = int(1e9 + 7)
-        @cache
+        
+        arrLen = min(arrLen,steps)
+        
+        dp = [[-1]*(steps + 1) for _ in range(arrLen)]
+        
         def helper(pos,steps):
             if(steps == 0 and pos == 0):
                 return 1
             
             if(steps == 0 or pos < 0 or pos == arrLen):
                 return 0
+            if(dp[pos][steps] != -1):
+                return dp[pos][steps]
             
             left = helper(pos - 1,steps - 1)
             right = helper(pos + 1,steps - 1)
             stay = helper(pos,steps - 1)
             
-            return (left + right + stay)%M
+            dp[pos][steps] = (left + right + stay)%M
+            
+            return dp[pos][steps]
         
         return helper(0,steps)
         
